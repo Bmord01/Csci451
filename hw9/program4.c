99c29527849d429a5cee81a6bf2a9f547d5b8568
Bmord01 <BdrMord001@gmail.com>
2018-11-10 17:27:34 -0600

    Change program 4

--- hw9/program4.c
+++ hw9/program4.c
@@ -13,47 +13,35 @@
 #include <sys/sem.h>
 
 pthread_mutex_t lock;
+bool next1=false,next2=false,next3=false;
 
 void *readData1(void *args){
     FILE *inpt,*outpt;
     int inSID = (int)args;
     key_t SKey = ftok("program1.c",1);
     int sid;
-    int prev1=0,prev2=0,current=0,result=-1;;
+    int prev1=0,count=0,current=0,result=-1;;
     while(1){
         if((sid=semget(SKey,1,0660))>-1){
+            pthread_mutex_lock(&lock);
             if((result=semctl(sid,0,GETVAL,NULL))!=1){
                 //printf("Inside of reading\n");
                 inpt=fopen("output1.txt","r");
+                while(inpt==NULL){
+                    inpt=fopen("output1.txt","r");
+                }
                 fscanf(inpt,"%d",&current);
                 fclose(inpt);
                 if(current!=prev1){
-                    if((result = semctl(inSID,0,GETVAL,NULL)==0)){
-                        if(prev1==prev2){
-                            outpt = fopen("output4.txt","a");
-                            fprintf(outpt,"%d%d",prev1,prev2);
-                            fclose(outpt);
-                            semctl(inSID,0,SETVAL,1);
-                        }
-                        else{
-                            outpt = fopen("output4.txt","a");
-                            fprintf(outpt,"%d",prev1);
-                            fclose(outpt);
-                            semctl(inSID,0,SETVAL,1);
-                            //write prev1 to output4.txt
-                        }
-                    }
+                    outpt=fopen("output4.txt","a");
+                    fprintf(outpt,"Thread 1: %d\n",current);
+                    printf("Thread 1: %d\n",current);
+                    fclose(outpt);
+                    
                     prev1=current;
                 }
-                else if(prev1!=prev2){
-                    prev2=prev1;
-                }
-                else{
-                    if((result=semctl(inSID,0,GETVAL,NULL))==0){
-                        semctl(inSID,0,SETVAL,1);
-                    }
-                }
             }
+            pthread_mutex_unlock(&lock);
         }
         usleep(500000);
     }
@@ -65,41 +53,27 @@ void *readData2(void *args){
     int inSID = (int)args;
     key_t SKey = ftok("program2.c",1);
     int sid;
-    int prev1=0,prev2=0,current=0,result=-1;;
+    int prev1=0,count=0,current=0,result=-1;;
     while(1){
         if((sid=semget(SKey,1,0660))>-1){
+            pthread_mutex_lock(&lock);
             if((result=semctl(sid,0,GETVAL,NULL))!=1){
                 //printf("Inside of reading\n");
                 inpt=fopen("output2.txt","r");
+                while(inpt==NULL){
+                    inpt=fopen("output1.txt","r");
+                }
                 fscanf(inpt,"%d",&current);
                 fclose(inpt);
                 if(current!=prev1){
-                    if((result = semctl(inSID,0,GETVAL,NULL)==1)){
-                        if(prev1==prev2){
-                            outpt = fopen("output4.txt","a");
-                            fprintf(outpt,"%d%d",prev1,prev2);
-                            fclose(outpt);
-                            semctl(inSID,0,SETVAL,0);
-                        }
-                        else{
-                            outpt = fopen("output4.txt","a");
-                            fprintf(outpt,"%d",prev1);
-                            fclose(outpt);
-                            semctl(inSID,0,SETVAL,0);
-                            //write prev1 to output4.txt
-                        }
-                    }
+                    outpt=fopen("output4.txt","a");
+                    fprintf(outpt,"Thread 2: %d\n",current);
+                    printf("Thread 2: %d\n",current);
+                    fclose(outpt);
                     prev1=current;
                 }
-                else if(prev1!=prev2){
-                    prev2=prev1;
-                }
-                else{
-                    if((result=semctl(inSID,0,GETVAL,NULL))==0){
-                        semctl(inSID,0,SETVAL,0);
-                    }
-                }
             }
+            pthread_mutex_unlock(&lock);
         }
         usleep(500000);
     }
@@ -111,41 +85,29 @@ void *readData3(void *args){
     int inSID = (int)args;
     key_t SKey = ftok("program3.c",1);
     int sid;
-    int prev1=0,prev2=0,current=0,result=-1;;
+    int prev1=0,count=0,current=0,result=-1;;
     while(1){
         if((sid=semget(SKey,1,0660))>-1){
+            pthread_mutex_lock(&lock);
             if((result=semctl(sid,0,GETVAL,NULL))!=1){
                 //printf("Inside of reading\n");
                 inpt=fopen("output3.txt","r");
+                while(inpt==NULL){
+                    inpt=fopen("output1.txt","r");
+                }
                 fscanf(inpt,"%d",&current);
                 fclose(inpt);
                 if(current!=prev1){
-                    if((result = semctl(inSID,0,GETVAL,NULL)==2)){
-                        if(prev1==prev2){
-                            outpt = fopen("output4.txt","a");
-                            fprintf(outpt,"%d%d",prev1,prev2);
-                            fclose(outpt);
-                            semctl(inSID,0,SETVAL,0);
-                        }
-                        else{
-                            outpt = fopen("output4.txt","a");
-                            fprintf(outpt,"%d",prev1);
-                            fclose(outpt);
-                            semctl(inSID,0,SETVAL,0);
-                            //write prev1 to output4.txt
-                        }
-                    }
+                    
+                    outpt=fopen("output4.txt","a");
+                    fprintf(outpt,"Thread 3: %d\n",current);
+                    printf("Thread 3: %d\n",current);
+                    fclose(outpt);
+                    
                     prev1=current;
                 }
-                else if(prev1!=prev2){
-                    prev2=prev1;
-                }
-                else{
-                    if((result=semctl(sid,0,GETVAL,NULL))==2){
-                        semctl(inSID,0,SETVAL,0);
-                    }
-                }
             }
+            pthread_mutex_unlock(&lock);
         }
         usleep(500000);
     }
@@ -159,10 +121,11 @@ int main(){
     int sid = semget(SKey,1,0660 | IPC_CREAT);
     printf("SID = %d\n",sid);
     semctl(sid,0,SETVAL,0);
+    usleep(1);
     pthread_create(&tids[0],NULL,readData1,((void *) sid));
     pthread_create(&tids[1],NULL,readData2,((void *) sid));
-    //pthread_create(&tids[2],NULL,readData3,((void *) sid));
-    for(i=0;i<2;i++){
+    pthread_create(&tids[2],NULL,readData3,((void *) sid));
+    for(i=0;i<3;i++){
         pthread_detach(tids[i]);
     }
     while(1);
