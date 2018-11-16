05a2af8e11acc05db8671bf392c0d62feae75c95
Bmord01 <BdrMord001@gmail.com>
2018-11-15 22:28:01 -0600

    final

--- hw9/program4.c
+++ hw9/program4.c
@@ -13,33 +13,34 @@
 #include <sys/sem.h>
 
 bool next1=false,next2=false,next3=false;
+pthread_mutex_t lock;
+bool started[3];
 
 void *readData1(void *args){
     FILE *inpt,*outpt;
     int inSID = (int)args;
     key_t SKey = ftok("program1.c",1);
     int sid;
-    int prev1=0,count=0,current=0,result=-1;
+    int prev1=-1,count=0,current=-1,result=-1;
     bool print=false;
+    while((access("output1.txt",F_OK))==-1);
+    started[0]=true;
     while(1){
         if((sid=semget(SKey,1,0660))>-1){
             if((result=semctl(sid,0,GETVAL,NULL))!=1){
                 //printf("Inside of reading\n");
                 inpt=fopen("output1.txt","r");
-                while(inpt==NULL){
-                    inpt=fopen("output1.txt","r");
-                }
                 fscanf(inpt,"%d",&current);
                 fclose(inpt);
                 if(current!=prev1){
                     print=true;
                 }
                 if(print){
-                    //while((result = semctl(inSID,0,GETVAL,NULL))!=0);
+                    while((result = semctl(inSID,0,GETVAL,NULL))!=0);
                     if((result = semctl(inSID,0,GETVAL,NULL))==0){
                         outpt=fopen("output4.txt","a");
-                        fprintf(outpt,"Thread 1: %d\n",current);
-                        printf("Thread 1: %d\n",current);
+                        fprintf(outpt,"%d ",current);
+                        //printf("Thread 1: %d\n",current);
                         fclose(outpt);
                         prev1=current;
                         print=false;
@@ -63,25 +64,23 @@ void *readData2(void *args){
     int sid;
     int prev1=0,count=0,current=0,result=-1;
     bool print =false;
+    while((access("output2.txt",F_OK))==-1 ||!started[0]);
+    started[1]=true;
     while(1){
         if((sid=semget(SKey,1,0660))>-1){
             if((result=semctl(sid,0,GETVAL,NULL))!=1){
                 //printf("Inside of reading\n");
                 inpt=fopen("output2.txt","r");
-                while(inpt==NULL){
-                    inpt=fopen("output1.txt","r");
-                }
                 fscanf(inpt,"%d",&current);
                 fclose(inpt);
                 if(current!=prev1){
                     print=true;
                 }
                 if(print){
-                    //while((result = semctl(inSID,0,GETVAL,NULL))!=1);
+                    while((result = semctl(inSID,0,GETVAL,NULL))!=1);
                     if((result = semctl(inSID,0,GETVAL,NULL))==1){
                         outpt=fopen("output4.txt","a");
-                        fprintf(outpt,"Thread 2: %d\n",current);
-                        printf("Thread 2: %d\n",current);
+                        fprintf(outpt,"%d ",current);
                         fclose(outpt);
                         prev1=current;
                         print=false;
@@ -100,31 +99,29 @@ void *readData2(void *args){
 }
 
 void *readData3(void *args){
-    FILE *inpt,*outpt;
     int inSID = (int)args;
     key_t SKey = ftok("program3.c",1);
     int sid;
-    int prev1=0,count=0,current=0,result=-1;
+    int prev1=-1,count=0,current=-1,result=-1;
     bool print=false;
+    FILE *inpt,*outpt;
+    while((access("output3.txt",F_OK))==-1 || !started[1] || !started[0]);
+    started[2]=true;
     while(1){
         if((sid=semget(SKey,1,0660))>-1){
             if((result=semctl(sid,0,GETVAL,NULL))!=1){
                 //printf("Inside of reading\n");
                 inpt=fopen("output3.txt","r");
-                while(inpt==NULL){
-                    inpt=fopen("output1.txt","r");
-                }
                 fscanf(inpt,"%d",&current);
                 fclose(inpt);
                 if(current!=prev1){
                     print=true;
                 }
                 if(print){
-                    //while((result = semctl(inSID,0,GETVAL,NULL))!=2);
+                    while((result = semctl(inSID,0,GETVAL,NULL))!=2);
                     if((result = semctl(inSID,0,GETVAL,NULL))==2){
                         outpt=fopen("output4.txt","a");
-                        fprintf(outpt,"Thread 3: %c\n",current);
-                        printf("Thread 3: %c\n",current);
+                        fprintf(outpt,"%c ",current);
                         fclose(outpt);
                         prev1=current;
                         print=false;
@@ -146,9 +143,8 @@ int main(){
     int i;
     key_t SKey = ftok("program4.c",1);
     int sid = semget(SKey,1,0660 | IPC_CREAT);
-    printf("SID = %d\n",sid);
+    //printf("SID = %d\n",sid);
     semctl(sid,0,SETVAL,0);
-    usleep(1);
     pthread_create(&tids[0],NULL,readData1,((void *) sid));
     pthread_create(&tids[1],NULL,readData2,((void *) sid));
     pthread_create(&tids[2],NULL,readData3,((void *) sid));
